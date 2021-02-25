00a5a2892787bc15b7afb72414f95744b825a5f9
Lucia Cerchie <luciacerchie@gmail.com>
2021-02-25 12:51:48 -0700

    adds semis to prettier and new cors config

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
-"use strict"
+"use strict";
 
-const express = require("express") //grabbing express
+const express = require("express"); //grabbing express
 
-const { NotFoundError } = require("./expressError") //going to need this for rendering routes
+const { NotFoundError } = require("./expressError"); //going to need this for rendering routes
 
-const { authenticateJWT } = require("./middleware/auth") //going to need this for rendering JWT
+const { authenticateJWT } = require("./middleware/auth"); //going to need this for rendering JWT
 
-const usersRoutes = require("./routes/users") //routes for user functionality
+const usersRoutes = require("./routes/users"); //routes for user functionality
 
-const morgan = require("morgan") //to help us read console output better
+const morgan = require("morgan"); //to help us read console output better
 
-const app = express() //revving up the app
+const app = express(); //revving up the app
 
-const cors = require("cors")
+const cors = require("cors");
 
 //registering our middleware below
-app.use(express.json())
-app.use(morgan("tiny"))
-app.use(authenticateJWT)
-app.use(cors({ origin: ["https://clever-kare-b13870.netlify.app"] }))
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authenticateJWT);
+
 //have to add more when I deploy on heroku
 
 //establishing first url subdirectories
 
-app.use("/users", usersRoutes)
+app.use("/users", usersRoutes);
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
-    return next(new NotFoundError())
-})
+    return next(new NotFoundError());
+});
+
+app.use(
+    cors({
+        origin: "https://clever-kare-b13870.netlify.app",
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        allowedHeaders: ["Content-Type", "Accept", "x-access-token", "X-Key"],
+    })
+);
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-    if (process.env.NODE_ENV !== "test") console.error(err.stack)
-    const status = err.status || 500
-    const message = err.message
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
     return res.status(status).json({
         error: { message, status },
-    })
-})
+    });
+});
 
-module.exports = app
+module.exports = app;
 //adapted from Springboard bootcamp setup https://www.springboard.com/workshops/software-engineering-career-track
